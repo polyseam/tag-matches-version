0d75ce994ac8c142fffedfefadec46f3af31eeb2
Matt Johnston <m@ttjohnston.com>
2025-03-06 08:19:51 -0400

    add fail-on-mismatch input

--- index.js
+++ index.js
@@ -13,20 +13,40 @@ function parseData(pathToData) {
   return jsonc.parse(data);
 }
 
-try {
-  // eg. package.json
-  const pathToData = core.getInput("file-path");
-  // eg. version
-  const key = core.getInput("key");
-  // eg. v
-  const tagPrefix = core.getInput("tag-prefix");
-  // eg 1.0.0
-  const tagWithoutPrefix = process.env.GITHUB_REF.replace(
-    `refs/tags/${tagPrefix}`,
-    "",
-  );
-  const data = parseData(pathToData);
-  core.setOutput("matches", data[key] === tagWithoutPrefix);
-} catch (error) {
-  core.setFailed(error.message);
+function run() {
+  try {
+    // eg. package.json
+    const pathToData = core.getInput("file-path");
+
+    // eg. version
+    const key = core.getInput("key");
+
+    // eg. v
+    const tagPrefix = core.getInput("tag-prefix");
+
+    // eg 1.0.0
+    const tagWithoutPrefix = process.env.GITHUB_REF.replace(
+      `refs/tags/${tagPrefix}`,
+      "",
+    );
+
+    // default true
+    const failOnMismatch = core.getInput("fail-on-mismatch");
+
+    const data = parseData(pathToData);
+
+    if (data[key] === tagWithoutPrefix) {
+      core.setOutput("matches", true);
+    } else if (failOnMismatch) {
+      core.setFailed(
+        `${pathToData}[${key}] does not match tag ${tagWithoutPrefix}`,
+      );
+    }
+
+    core.setOutput("matches", false);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
